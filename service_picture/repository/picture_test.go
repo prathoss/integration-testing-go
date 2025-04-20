562a8293dce11bd11f52a93e8dd41f33bac7f882
Michal Bradáč <michal.bradac@oddin.gg>
2025-04-20 18:17:39 +0200

    add lots of read tests

---
+++ service_picture/repository/picture_test.go
@@ -0,0 +1,66 @@
+package repository_test
+
+import (
+	"cmp"
+	"fmt"
+	"slices"
+	"testing"
+	"time"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/prathoss/integration_testing/domain"
+	"github.com/prathoss/integration_testing/service_picture/repository"
+	"github.com/prathoss/integration_testing/test/deps"
+)
+
+func TestPictureRepository(t *testing.T) {
+	for i := range 100 {
+		t.Run(
+			fmt.Sprintf("query by author: %d", i), func(t *testing.T) {
+				GetByAuthorID(t)
+			},
+		)
+	}
+}
+
+func GetByAuthorID(t *testing.T) {
+	ctrl := deps.NewBuilder(t).
+		WithPG(t).
+		Build()
+
+	pool, err := pgxpool.New(t.Context(), ctrl.GetPG().GetAddress(t))
+	if err != nil {
+		t.Fatalf("failed to create pool: %v", err)
+	}
+	defer pool.Close()
+
+	pictureRepo := repository.NewPicture(pool)
+	pictures, err := pictureRepo.GetByAuthorID(t.Context(), 1)
+	if err != nil {
+		t.Fatalf("could not get pictures by author: %v", err)
+	}
+	expected := []domain.Picture{
+		{
+			ID:        1,
+			AuthorID:  1,
+			URL:       "https://example.com/images/sunset.jpg",
+			ViewCount: 5,
+			CreatedAt: time.Date(2025, 4, 14, 21, 9, 43, 0, time.UTC),
+		},
+		{
+			ID:        2,
+			AuthorID:  1,
+			URL:       "https://example.com/images/mountain.jpg",
+			ViewCount: 1,
+			CreatedAt: time.Date(2025, 4, 14, 21, 10, 43, 0, time.UTC),
+		},
+	}
+	slices.SortFunc(
+		pictures, func(a, b domain.Picture) int {
+			return cmp.Compare(a.ID, b.ID)
+		},
+	)
+	if !slices.Equal(expected, pictures) {
+		t.Errorf("expected: %v, got: %v", expected, pictures)
+	}
+}
--- service_picture/repository/picture_view_test.go
+++ service_picture/repository/picture_view_test.go
@@ -1,66 +1,46 @@
 package repository_test
 
 import (
-	"errors"
 	"testing"
 	"time"
 
 	"github.com/jackc/pgx/v5/pgxpool"
-	"github.com/prathoss/integration_testing/domain"
 	"github.com/prathoss/integration_testing/service_picture/repository"
 	"github.com/prathoss/integration_testing/test/deps"
 )
 
 func TestPictureViewRepository_GetByProfileAndPicture(t *testing.T) {
+	for range 100 {
+		t.Run(
+			"Success", func(t *testing.T) {
+				// Create a mock DB connection
+				ctrl := deps.NewBuilder(t).
+					WithPG(t).
+					Build()
 
-	t.Run(
-		"Success", func(t *testing.T) {
-			// Create a mock DB connection
-			ctrl := deps.NewBuilder(t).
-				WithPG(t).
-				Build()
-			pool, err := pgxpool.New(t.Context(), ctrl.GetPG().GetAddress(t))
-			if err != nil {
-				t.Fatalf("could not connect to database: %v", err)
-			}
-			repo := repository.NewView(pool)
+				pool, err := pgxpool.New(t.Context(), ctrl.GetPG().GetAddress(t))
+				if err != nil {
+					t.Fatalf("could not connect to database: %v", err)
+				}
+				repo := repository.NewView(pool)
 
-			view, err := repo.GetByProfileAndPicture(t.Context(), 3, 4)
-			if err != nil {
-				t.Fatalf("GetByProfileAndPicture returned error: %v", err)
-			}
-			expected := repository.PictureView{
-				ID:           5,
-				ProfileID:    3,
-				PictureID:    4,
-				ViewCount:    5,
-				LastViewedAt: time.Date(2025, 4, 14, 22, 13, 43, 0, time.UTC),
-			}
-			if !equalPictureView(view, expected) {
-				t.Fatalf("expected %+v, got %+v", expected, view)
-			}
-		},
-	)
-
-	t.Run(
-		"Not Found", func(t *testing.T) {
-			// Create a mock DB connection
-			ctrl := deps.NewBuilder(t).
-				WithPG(t).
-				Build()
-			pool, err := pgxpool.New(t.Context(), ctrl.GetPG().GetAddress(t))
-			if err != nil {
-				t.Fatalf("could not connect to database: %v", err)
-			}
-			repo := repository.NewView(pool)
-
-			_, err = repo.GetByProfileAndPicture(t.Context(), 0, 0)
-			var enf *domain.ErrNotFound
-			if !errors.As(err, &enf) {
-				t.Fatalf("expected to get not found error, got %T", err)
-			}
-		},
-	)
+				view, err := repo.GetByProfileAndPicture(t.Context(), 3, 4)
+				if err != nil {
+					t.Fatalf("GetByProfileAndPicture returned error: %v", err)
+				}
+				expected := repository.PictureView{
+					ID:           5,
+					ProfileID:    3,
+					PictureID:    4,
+					ViewCount:    5,
+					LastViewedAt: time.Date(2025, 4, 14, 22, 13, 43, 0, time.UTC),
+				}
+				if !equalPictureView(view, expected) {
+					t.Fatalf("expected %+v, got %+v", expected, view)
+				}
+			},
+		)
+	}
 }
 
 func equalPictureView(a, b repository.PictureView) bool {
